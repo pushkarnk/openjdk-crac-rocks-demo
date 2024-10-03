aa8be86a6d3f9d586a8f230965199d4fb5f64c37
Pushkar Niranjan Kulkarni <pushkar.kulkarni@canonical.com>
2024-10-04 01:15:18 +0530

    Update start.sh

--- checkpoint/scripts/start.sh
+++ checkpoint/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..9999}; do
-  sleep $i &
+  sleep $i &  # Avoid PID collisions during restore
 done
 /usr/bin/java -XX:CRaCCheckpointTo=/var/lib/pebble/default/crac-files -jar /jars/spring-petclinic.jar
